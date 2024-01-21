8eacea1a635bd40fa236c56d0c39b9c51ce9c59b
JovemDevv <anacarolinesan18@gmail.com>
2024-01-21 15:51:55 -0300

    criando a extração bancaria por data

--- src/index.js
+++ src/index.js
@@ -71,4 +71,40 @@ app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
 
   return response.status(201).send();
 });
+
+app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
+  const { amount } = request.body;
+  const { customer } = resquest;
+
+  const balance = getBalance(customer.statement);
+
+  if (balance < amount) {
+    return response.status(400).json({ error: "Insufficient funds!" });
+  }
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: "debit",
+  };
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+});
+
+app.get("/statement/date", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  const { date } = request.query;
+
+  const dateFormat = new Date(date + " 00:00");
+
+  const statement = customer.statement.filter(
+    (statement) =>
+      statement.created_at.toDateString() ===
+      new Date(dateFormat).toDateString()
+  );
+
+  return response.json(statement);
+});
+
 app.listen(3030);
